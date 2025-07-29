179fd23ec7992ea2b513dcb104f1cd7333ea3224
VamshiMaheshFyntune <maheshunivamshi.fyntune@gmail.com>
2025-07-29 10:51:01 +0530

    comit

--- src/main/java/com/ims/entity/SalesItem.java
+++ src/main/java/com/ims/entity/SalesItem.java
@@ -31,7 +31,7 @@ public class SalesItem {
         this.quantity = quantity;
     }
 
-    public Long getSalesItemId() {
+    public Long getSalesItemId()                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            {
         return salesItemId;
     }
 
